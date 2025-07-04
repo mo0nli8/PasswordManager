252532df0478f6e4db73a4fbb5a75a58eac79eb8
arsenii.zagriichuk <arsenzagriichuk@student.griffith.ie>
2025-07-04 19:22:58 +0100

    created basic skeleton for the project

---
+++ src/main/java/com/example/passwordmanager/Main.java
@@ -0,0 +1,28 @@
+package com.example.passwordmanager;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/passwordmanager/mainScene.fxml"));
+            Parent root = loader.load();
+
+            primaryStage.setTitle("Password Manager");
+            primaryStage.setScene(new Scene(root, 800, 500)); // Adjust size as needed
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
--- src/main/java/com/example/passwordmanager/dataController.java
+++ src/main/java/com/example/passwordmanager/dataController.java
@@ -1,5 +1,45 @@
 package com.example.passwordmanager;
 
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class dataController {
 
+    //attributes for the class
+    @FXML private TextField websiteField;
+    @FXML private TextField userNameField;
+    @FXML private PasswordField passwordField;
+    @FXML private Button saveButton;
+
+    // list for saving the data (allows to track changes when they occur)
+    private ObservableList<appData> entryList;
+
+
+    public void initialize() {
+        saveButton.setOnAction(event -> handleSave());
+    }
+
+    //setting the list
+    public void setEntryList(ObservableList<appData> entryList) {
+        this.entryList = entryList;
+    }
+
+    // a method to save the data to the list
+    private void handleSave(){
+        // putting the data into the variables
+        String website = websiteField.getText();
+        String userName = userNameField.getText();
+        String password = passwordField.getText();
+
+        appData data = new appData(userName,password,website);
+        entryList.add(data);
+    }
+
+
+
+
 }
+
